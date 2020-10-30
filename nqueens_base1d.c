6f00470508251995f19c24d7e6d54e6f6e14fcd1
Guangy_li <guangy.li@hotmail.com>
2020-10-30 12:59:45 -0400

    add comment on the baseline version

--- nqueens_base1d.c
+++ nqueens_base1d.c
@@ -7,9 +7,11 @@ backtracking */
 #include <string.h>
 #define BILLION 1000000000L 
 
-int best_profit, *best_board, sol_num;
+int best_profit, *best_board, sol_num; // global variables used by all threads
 
-/* A utility function to print solution */
+/////////////////////////////////////Utility Functions/////////////////////////////////////
+
+/* A utility function to print solution in 2D visual representation */
 void printSolution(int n, int *board) 
 { 
   int i,j;
@@ -23,7 +25,7 @@ void printSolution(int n, int *board)
   printf("\n");
 } 
 
-/* Copies src n x n matrix into dest n x n matrix */
+/* Copies src array into dest array */
 void copy(int n, int *src, int *dest)
 {
   int i;
@@ -35,28 +37,35 @@ int totalProfit(int n, int *board)
 {
   int i, prof;
   prof = 0;
-  for (i = 0; i < n; i++) prof += abs(i - board[i]);
+  
+  //i represents column number whereas board[i] represent row number
+  for (i = 0; i < n; i++) prof += abs(i - board[i]); 
   
   return prof;
 }
     
 /* Returns true if the board satisfies the n queens problem if a queen 
    were to be inserted at row r and column c */
-bool isSafe(int r, int *board, int c) 
+bool isSafe(int r, int *board, int col) 
 { 
   int i;
-	// return false if two queens share the same column
-	for (i = 0; i < c; i++){
-		if ((board[i]==r) || (c-i==abs(board[i]-r))) return false;
+  // return false if two queens share the same column
+  for (i = 0; i < col; i++){
+    if ((board[i]==r) || (col-i==abs(board[i]-r))) return false;
   }
-	return true;
+  return true;
 } 
 
+/////////////////////////////////////Recursive Function/////////////////////////////////////
+
+// The recursive function that solve the nqueen problem using backtracking
 void solvenq(int n, int *board, int c) 
 {
   // base case for when all queens have been inserted
   if (c == n) {
-    sol_num++;
+    sol_num++; // The solution counter 
+	
+	// Copy the current board to the memory allocated to best_board if the current profit is larger than the store best case
     int temp;
     temp = totalProfit(n, board);
     if (temp > best_profit) {
@@ -65,23 +74,27 @@ void solvenq(int n, int *board, int c)
     }
     return;
   }
+  
+  // Iterate from row 0 to n to place all acceptable queens on the current column
   int i;
   for (i = 0; i < n; i++) {
     board[c] = i;
-		// if no two queens threaten each other
-		if (isSafe(i, board, c)) solvenq(n, board, c+1);
-	}
+    // if no two queens threaten each other
+    if (isSafe(i, board, c)) solvenq(n, board, c+1);
+  }
 }
 
+/////////////////////////////////////Main Code/////////////////////////////////////
+// Calculates execution time and perform nqueen calculation
 int main(int argc, char **argv) 
 { 
   struct timespec start, end;
   double time;
   int n, i, j, *board;
   if(argc != 2) {
-        printf("Usage: nqueens_baseline n\nAborting...\n");
-        exit(0);
-    }
+    printf("Usage: nqueens_baseline n\nAborting...\n");
+    exit(0);
+  }
   n = atoi(argv[1]);
   
   sol_num = 0;
@@ -90,12 +103,12 @@ int main(int argc, char **argv)
   board = (int *) malloc(n * sizeof(int));
   best_board = (int *) malloc(n * sizeof(int));
   for (i = 0; i < n; i++) {
-      board[i] = 0;
-      best_board[i] = 0;
+    board[i] = 0;
+    best_board[i] = 0;
   }
 
   // calls program to run while tracking execution time
-	clock_gettime(CLOCK_MONOTONIC, &start);
+  clock_gettime(CLOCK_MONOTONIC, &start);
   solvenq(n, board, 0);
   clock_gettime(CLOCK_MONOTONIC, &end);
     
@@ -112,5 +125,5 @@ int main(int argc, char **argv)
   // frees all allocated memory
   free(board);
   free(best_board);
-	return 0; 
+  return 0; 
 } 
